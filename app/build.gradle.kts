57303ca1293282a59df5601de7ffcd8902a89e48
Atuy <130917473+atuy1219@users.noreply.github.com>
2025-09-29 14:31:42 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,9 +34,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
+
     buildFeatures {
         compose = true
     }
