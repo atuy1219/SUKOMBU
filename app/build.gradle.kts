7bd7f3d67af7b2337109400fdedfc63aa66e2c15
atuy1219 <yuta1219aka@gmail.com>
2025-11-24 17:43:45 +0900

    バージョン番号を更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,8 +56,8 @@ android {
         applicationId = "com.atuy.scomb"
         minSdk = 34
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GIT_COMMIT_HASH", "\"${getGitCommitHash()}\"")
