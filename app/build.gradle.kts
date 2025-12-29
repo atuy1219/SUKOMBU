f423a30c0f3aafff42a1de7d44815a357856a117
Atuy <130917473+atuy1219@users.noreply.github.com>
2025-12-29 12:37:29 +0000

    ビルドエラー修正

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -133,6 +133,7 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.ui)
+    implementation("androidx.compose.animation:animation")
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
